265090d229cfa9d3c44cd1dbfd948ab1c7a1c535
Sam Ragsdale <lunesazul@duck.com>
2024-10-24 19:07:21 -0400

    Initial commit

---
+++ sragsdale_module31-6.py
@@ -0,0 +1,13 @@
+'''
+Author: Samuel Ragsdale
+Date completed: 24 October 2024
+Description: Module 3.1 - 3.6 from the text book Introducing Python 2nd edition
+In this program I demonstrate the use of variables and math functions
+native to Python. Starting with calculating the number of seconds in
+an hour, assigning that number to a variable and using that variable
+to work out how many seconds in a day (also in a variable). Finally,
+using division to determine how many hours in a day using floating point 
+and integer division.
+'''
+
+
